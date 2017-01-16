270ae216e2982f0efccff43e0472bf5174db485d
Marc Fournier <marc.fournier91@gmail.com>
2017-01-16 01:45:47 -0500

    Jello world

---
+++ Assets/TextController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TextController : MonoBehaviour {
+
+	public Text text;
+	// Use this for initialization
+	void Start () {
+		text.text = "Jello world";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)){
+			text.text = "Space key pressed";
+		}
+	}
+}
